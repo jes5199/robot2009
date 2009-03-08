a384434188cfacd1f17259225c4aca2f20b6965e
Jesse Wolfe <jes5199@electra.jes5199.com>
2009-03-08 15:21:53 -0700

    Let's see if we can spin that servo.

---
+++ rotate.c
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo axis;
+
+void setup(){
+	axis.attach(7);
+}
+
+void loop(){
+	static int axis_angle = 0;
+
+	Servo::refresh();
+
+	axis_angle += 1;
+	axis_angle %= 180;
+	axis.write(axis_angle)
+}
