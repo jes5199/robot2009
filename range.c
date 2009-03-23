77efde9169fb83eaa3378a722f8d097a08c44d98
Monica Toth <mh.toth@gmail.com>
2009-03-22 22:27:04 -0700

    This is the file that actually works

--- range.c
+++ range.c
@@ -7,7 +7,11 @@ unsigned int measure_distance()
    digitalWrite(SONAR_TRIGGER_PIN, HIGH);
    delayMicroseconds(10);
    digitalWrite(SONAR_TRIGGER_PIN, LOW);
-
+   
+   if
+   (digitalRead(SONAR_ECHO_PIN)==HIGH)
+   {Serial.println("fucked!");}
+   
    // wait for the pulse to return. The pulse
    // goes from low to HIGH to low, so we specify
    // that we want a HIGH-going pulse below:
@@ -15,10 +19,11 @@ unsigned int measure_distance()
 
    // can't send more than one pulse each 50 ms or
    // we could get interference
-   delay(50);
+   delay(500);
 
    // convert to inches and return
    return( (unsigned int) (pulse_length / 148) );
+   // return(pulse_length);
 }
 
 
@@ -26,10 +31,13 @@ void setup()
 {
    pinMode(SONAR_TRIGGER_PIN,          OUTPUT);
    pinMode(SONAR_ECHO_PIN,             INPUT);
+   Serial.begin(9600);
+   Serial.println("hellooooo");
 }
 
 
 void loop()
 {
    unsigned int current_distance = measure_distance();
+   Serial.println(current_distance);
 }
